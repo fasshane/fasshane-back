a3d3b7a3d8fbf5eced889731f2ccc85a621fa5a9
Hontar Mykhailo <m.hontar@umber.tech>
2025-05-15 10:55:19 +0300

    fix style

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { env } from './config';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { env } from "./config";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  );
   const port = env.PORT ?? 3001;
   await app.listen(port, () => {
     console.log(`http://localhost:${port}`);
   });
-
 }
 
 bootstrap();
