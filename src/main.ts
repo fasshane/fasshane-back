aea66523d3fec46ab2a7cecb50b32be3dcf82c6c
Hontar Mykhailo <m.hontar@umber.tech>
2025-09-14 09:36:36 +0300

    remove enableCors static

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // TODO: delete it
-  app.enableCors({
-    origin: 'http://localhost:3000', // your React app origin
-    credentials: true,               // allow cookies/auth headers
-  });
-
-  // app.enableCors();
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
